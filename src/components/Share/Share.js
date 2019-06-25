6817dbe221f3e241d40e822ea7cd42d4a488b62b
Dave Mihalcik <dmihalcik@virtru.com>
2019-06-25 16:58:28 -0400

    ♻️ Loading...
    * Adds a loading spinner while uploading the helloworld doc.
    * adds a done state and kicks off another list
    * doesn't seem to work quite right yet

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -5,30 +5,35 @@ import { init as shareable } from './services/gsuite';
 
 /* global gapi */
 
+// function sleep(ms) {
+//   return new Promise(resolve => setTimeout(resolve, ms));
+// }
+
 function Share() {
+  const [shareState, setShareState] = useState('unshared');
   const [data, setData] = useState({ status: 'loading', files: [] });
+
+  // Run this periodically or after uploading a file
+  const list = async () => {
+    const response = await gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': "nextPageToken, files(id, name)"
+    });
+    setData({files: response.result.files});
+  };
 
   useEffect(() => {
     async function startShare() {
       await shareable();
-
-      // Run this periodically
-      const list = async () => {
-        const response = await gapi.client.drive.files.list({
-            'pageSize': 10,
-            'fields': "nextPageToken, files(id, name)"
-        });
-        setData({files: response.result.files});
-      };
       
       const resetAuthButtons = async () => {
         const signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        setData({ status: (signedIn ? '' : 'un') + 'authorized' });
+        setShareState('unshared');
+
         if (signedIn) {
           list();
         }
-        setData({
-          status: signedIn ? 'authorized' : 'unauthorized'
-        });
       };
       resetAuthButtons();
       gapi.auth2.getAuthInstance().isSignedIn.listen(resetAuthButtons);
@@ -81,9 +86,42 @@ function Share() {
       };
     };
 
+    setShareState('sharing');
     const response = await gapi.client.request(makeRequest('helloworld.txt', 'text/plain', 'Hello World!'));
+    // if we need to debug the spinner add this
+    // await sleep(4000);
+    setShareState('shared');
     console.log(response);
+    list();
   };
+
+  function ShareButton() {
+    return <>
+      <FormBoxInstruction>Write a simple file to drive</FormBoxInstruction>
+      <button id="upload_button"
+              className="Share-upload-google"
+              onClick={upload}>upload</button>
+    </>;
+  }
+
+  function ShareUploading() {
+    return <div className="Share-file-uploading"></div>;
+  }
+
+  function ShareDone(props) {
+    return <div className="Share-done">{ props.children }</div>;
+  }
+
+  function ShareStatus() {
+    console.log('ShareStatus: ' + JSON.stringify(shareState));
+    switch (shareState) {
+      case 'unshared': return <ShareButton />;
+      case 'sharing': return <ShareUploading />;
+      // TODO(dmihalcik): link to drive folder.
+      case 'shared': return <ShareDone>DONE</ShareDone>;
+      default: return <h3>shareState = [{shareState}]</h3>;
+    }
+  }
 
   function renderButton() {
     return data.status === 'loading'
@@ -94,10 +132,7 @@ function Share() {
                     onClick={authorize}>Authorize</button>
           : 
             <>
-              <FormBoxInstruction>Write a simple file to drive</FormBoxInstruction>
-              <button id="upload_button"
-                      className="Share-upload-google"
-                      onClick={upload}>upload</button>
+              <ShareStatus share={data.share} />
               <FormBoxAlternative>Or</FormBoxAlternative>
               <button id="signout_button"
                       className="Share-signout-google"
