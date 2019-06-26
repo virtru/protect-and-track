e81480faa97223c7a89ca1a6fe406451d5fb976c
Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>
2019-06-26 15:03:51 -0400

    ✨ Adds a button that uploads 'helloworld.txt' (#3)
    * Adds an `upload` function to the `gsuite` service library.
    * Demonsrates its use. 
    * Adds a loading spinner while uploading a helloworld doc.
    * Updates auth so the loading works and displays a more informative message.

--- src/components/Share/Share.js
+++ src/components/Share/Share.js
@@ -1,34 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { FormBox, FormBoxAlternative } from 'components/FormBox/FormBox';
+import { FormBox, FormBoxInstruction, FormBoxAlternative } from 'components/FormBox/FormBox';
 import './Share.css';
-import { init as shareable } from './services/gsuite';
+import { init as shareable, upload as uploadToDrive } from './services/gsuite';
 
 /* global gapi */
 
 function Share() {
+  const [shareState, setShareState] = useState('unshared');
   const [data, setData] = useState({ status: 'loading', files: [] });
+
+  // Run this periodically or after uploading a file
+  const list = async () => {
+    const response = await gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': "nextPageToken, files(id, name)"
+    });
+    setData({status: 'authorized', files: response.result.files});
+  };
 
   useEffect(() => {
     async function startShare() {
       await shareable();
-
-      // Run this periodically
-      const list = async () => {
-        const response = await gapi.client.drive.files.list({
-            'pageSize': 10,
-            'fields': "nextPageToken, files(id, name)"
-        });
-        setData({files: response.result.files});
-      };
       
       const resetAuthButtons = async () => {
         const signedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
+        setData({
+            status: signedIn ? 'authorized' : 'unauthorized'	
+        });
+        setShareState('unshared');
+
         if (signedIn) {
           list();
         }
-        setData({
-          status: signedIn ? 'authorized' : 'unauthorized'
-        });
       };
       resetAuthButtons();
       gapi.auth2.getAuthInstance().isSignedIn.listen(resetAuthButtons);
@@ -36,40 +39,89 @@ function Share() {
     startShare();
   }, []);
 
-  const authorize = () => {
-    gapi.auth2.getAuthInstance().signIn();
+  const authorize = async () => {
+    setData({ status: 'loading' });
+    await gapi.auth2.getAuthInstance().signIn();
+    setData({ status: 'authorized' });
   };
 
   const signOut = () => {
     gapi.auth2.getAuthInstance().signOut();
   };
 
-  function renderButton() {
-    return data.status === 'loading'
-          ? <FormBoxAlternative>loading...</FormBoxAlternative>
-        : data.status === 'unauthorized'
-          ? <button id="authorize"
-                    className="Share-authorize-google"
-                    onClick={authorize}>Authorize</button>
-          : <button id="signout_button"
-                    className="Share-signout-google"
-                    onClick={signOut}>Sign Out</button>;
+  const upload = async () => {
+    setShareState('sharing');
+    const response = await uploadToDrive('helloworld.txt', 'text/plain', 'Hello World!');
+    setShareState('shared');
+    console.log(response);
+    list();
+  };
+
+  const ShareButton = () =>
+      <>
+        <FormBoxInstruction>Write a simple file to drive</FormBoxInstruction>
+        <button id="upload_button"
+                className="Share-upload-google"
+                onClick={upload}>upload</button>
+      </>;
+  const ShareUploading = () =>
+      <div className="Share-file-uploading" />;
+  const ShareDone = props =>
+      <div className="Share-done">{ props.children }</div>;
+  const ShareStatus = () => {
+    switch (shareState) {
+      case 'unshared': return <ShareButton />;
+      case 'sharing': return <ShareUploading />;
+      // TODO(dmihalcik): link to drive folder.
+      case 'shared': return <ShareDone>DONE</ShareDone>;
+      default: return <h3>shareState = [{shareState}]</h3>;
+    }
   }
+
+  const AuthButton = () =>
+      <button id="authorize"
+              className="Share-authorize-google"
+              onClick={authorize}>
+        Authorize
+      </button>;
+  const AuthLoading = () =>
+      <FormBoxAlternative>
+        Awaiting authorization...
+      </FormBoxAlternative>;
+  const ShareOrSignOut = () =>
+      <>
+        <ShareStatus share={data.share} />
+        <FormBoxAlternative>Or</FormBoxAlternative>
+        <button id="signout_button"
+                className="Share-signout-google"
+                onClick={signOut}>Sign Out</button>
+      </>;
+
+  const AuthAndShareButton = () => {
+    switch (data.status) {
+      case 'unauthorized': return <AuthButton />;
+      case 'authorized': return <ShareOrSignOut />;
+      default: return <AuthLoading />;
+    }
+  }
+
+  const LastTenFiles = () => 
+      (data.files && data.files.length &&
+          <pre>
+            {
+              data.files.map((file) =>
+                <>
+                  {file.name + '\n'}
+                </>
+              )
+            }
+          </pre>)
+          || null;
 
   return (
     <FormBox title="Sharing Provider Tool" onSubmit={ e => { e.preventDefault(); } }>
-      { renderButton() }
-      {data.files && data.files.length &&
-        <pre>
-          {
-            data.files.map((file) =>
-              <>
-                {file.name + '\n'}
-              </>
-            )
-          }
-        </pre>
-      }
+      <AuthAndShareButton />
+      <LastTenFiles />
     </FormBox>
   );
 }
