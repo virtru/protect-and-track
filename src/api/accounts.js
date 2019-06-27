4d65af1a870bf2024bc3ba2ce1c407e253dd7430
NathanAB <nathan@virtru.com>
2019-06-27 12:39:24 -0400

    Update src/api/accounts.js
    Co-Authored-By: Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>

--- src/api/accounts.js
+++ src/api/accounts.js
@@ -1,5 +1,6 @@
 const USE_SINGLE_ENDPOINT = false;
 
+// TODO(DSAT-13): Pick the appropriate backend for the environment
 const ACCOUNTS_URL = USE_SINGLE_ENDPOINT ? 'https://api-develop01.develop.virtru.com/accounts' : 'https://accounts-develop01.develop.virtru.com';
 const ACCOUNTS_APPID_BUNDLE_URL = `${ACCOUNTS_URL}/api/currentAppIdBundle`;
 const LOGIN_URL = `${ACCOUNTS_URL}/login?loginPlatform=tdf-demo&loginRedirectUrl=${window.location}`;
