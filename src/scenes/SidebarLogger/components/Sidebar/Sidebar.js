ddfe42589cdeb4eaa52110b42f8b57aa627d58a2
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-06-27 19:00:55 +0300

    Need to install css modules

--- src/scenes/SidebarLogger/components/Sidebar/Sidebar.js
+++ src/scenes/SidebarLogger/components/Sidebar/Sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css';
 import { Scrollbars } from 'react-custom-scrollbars';
+import SidebarItem from './components/SidebarItem';
 
 export default ({collapse}) => (
   <div className="SidebarContainer">
@@ -8,8 +9,8 @@ export default ({collapse}) => (
       <span onClick={collapse} className="collapse">Hide</span>
     </div>
     <div className="itemWrapper">
-      <Scrollbars>
-
+      <Scrollbars autoHide>
+        <SidebarItem />
       </Scrollbars>
     </div>
   </div>
--- src/scenes/SidebarLogger/components/Sidebar/components/SidebarItem.js
+++ src/scenes/SidebarLogger/components/Sidebar/components/SidebarItem.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
+import './SidebarItem.css';
+
 export default () => (
-  <div></div>
+  <div>
+    <div className="json-wrapper">
+      <div className="showPayload">Show payload</div>
+      <pre className="json">{JSON.stringify({
+        main: 'line-height:1.3;color:#66d9ef;background:#272822;overflow:auto;',
+        key: 'color:#f92672;',
+        string: 'color:#fd971f;',
+        value: 'color:#a6e22e;',
+        boolean: 'color:#ac81fe;',
+      }, null, 2) }</pre>
+    </div>
+  </div>
 )
