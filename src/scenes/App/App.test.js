60c6db3df9001d30ca6843a286b5158415a0efca
Tucker <47113451+virtru-tdewitt@users.noreply.github.com>
2019-07-08 12:58:15 -0500

    DSAT-8: Create Buildkite pipeline (#13)
    * DSAT-8: pipeline
    
    * skip some things
    
    * build
    
    * remove license check
    
    * quote consistency and build command
    
    * this probably will not work...
    
    * env
    
    * artifact
    
    * test invalidation
    
    * only prod right now
    
    * deploy?
    
    * yaml
    
    * move wait
    
    * Add homepage
    
    * Enable CI testing, force audit for now
    
    * Fix tests
    
    * Combined linting with test step
    
    * appease the react hook gods
    
    * Update readme
    
    * Add deploy blocker
    
    * This should trigger a build
    
    * Update .buildkite/pipeline.yml
    
    Co-Authored-By: Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>
    
    * Update .buildkite/pipeline.yml
    
    Co-Authored-By: Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>

--- src/scenes/App/App.test.js
+++ src/scenes/App/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Pure as App } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  test('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App appIdBundle={false} setAppIdBundle={() => {}} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
