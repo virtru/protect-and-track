86b4ceecd7383ee4c7df79d54b81282f9d70eca5
Nathan Bakirci <nathan@virtru.com>
2019-07-09 11:28:36 -0400

    Remove comments

--- src/scenes/Sidebar/Sidebar.js
+++ src/scenes/Sidebar/Sidebar.js
@@ -3,13 +3,8 @@ import { connect } from 'redux-zero/react';
 import './Sidebar.css';
 
 import SdkLogger from './components/SdkLogger/SdkLogger';
-import useIsPortrait from '../../commonHooks/useIsPortrait';
 
 const Sidebar = ({ tdfLog }) => {
-  // const isPortrait = useIsPortrait();
-
-  // const contentToRender = <Sidebar events={tdfLog} />
-  // return !isPortrait && contentToRender;
   return (
     <div className="sidebarWrapper">
       <h3>Tracked events</h3>
