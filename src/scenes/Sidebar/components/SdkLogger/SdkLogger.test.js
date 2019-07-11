5f74cc4c39f82850a324ae016a69ff34e7316138
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-07-11 16:23:23 +0300

    Sdk logger covered

---
+++ src/scenes/Sidebar/components/SdkLogger/SdkLogger.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import SdkLogger from './SdkLogger';
+
+describe('Sidebar', () => {
+  test('Should trigger window.PR.prettyPrint every time component renders', () => {
+    render(<SdkLogger collapse={() => {}} events={[]} />);
+    expect(window.PR.prettyPrint).toHaveBeenCalled();
+  });
+});
