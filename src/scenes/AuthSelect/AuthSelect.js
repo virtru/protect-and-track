face34d4b3ed7b62d8a8337124da00e8c334f95c
Nathan Bakirci <nathan@virtru.com>
2019-07-18 10:45:26 -0400

    Lint ignore

--- src/scenes/AuthSelect/AuthSelect.js
+++ src/scenes/AuthSelect/AuthSelect.js
@@ -9,6 +9,7 @@ const AUTH_STEPS = {
 };
 
 function validateEmail(email) {
+  // eslint-disable-next-line no-useless-escape
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
