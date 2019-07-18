0a2b62232869f04e4f14a6232470a76396a64c41
ivanovSPvirtru <sivanov.ctr@virtru.com>
2019-07-18 16:29:44 +0300

    Document encryprtion and auth flow covered

--- src/scenes/Document/Document.test.js
+++ src/scenes/Document/Document.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { render, wait, fireEvent } from '@testing-library/react';
+import { render, wait, fireEvent, getByTestId, act } from '@testing-library/react';
 import Document from './Document';
-import { ENCRYPT_STATES } from './scenes/Policy/Policy';
 import * as tdf from 'utils/tdfWrapper';
+import * as services from 'services/audit';
 jest.mock('utils/tdfWrapper');
+jest.mock('services/audit');
 
 describe('Document', () => {
   test('should trigger auth and and updates Virtru Client on every update, if userId defined but there is no virtruClient Document ', async () => {
@@ -32,5 +33,87 @@ describe('Document', () => {
     const { getByText } = render(<Document file={file} />);
     fireEvent.click(getByText('Sign in to continue'));
     expect(getByText('Enter your email address:')).toBeInTheDocument();
+  });
+
+  test('should trigger loginAs on auth form submit, that will call tdf.authenticate and will update userId and virtru client', async () => {
+    const client = { userId: 'foo' };
+    const updateVirtruClient = jest.fn();
+    const updateUserId = jest.fn();
+    tdf.authenticate.mockImplementation(() => Promise.resolve(client));
+    const file = { file: { name: 'foo.txt' } };
+
+    const { container, getByText } = render(
+      <Document file={file} updateVirtruClient={updateVirtruClient} updateUserId={updateUserId} />,
+    );
+    fireEvent.click(getByText('Sign in to continue'));
+
+    fireEvent.change(getByTestId(container, 'emailAuthInput'), {
+      target: { value: 'foo@bar.com' },
+    });
+    fireEvent.submit(getByTestId(container, 'formAuth'));
+
+    act(() => {
+      // still throws warning. Should be fixed in later react and react testing lib versions
+      // https://github.com/testing-library/react-testing-library/issues/281#issuecomment-507910126
+      fireEvent.click(getByTestId(container, 'emailAuthButton'));
+    });
+
+    await wait(() => {
+      expect(updateVirtruClient).toHaveBeenCalledWith(client);
+      expect(updateUserId).toHaveBeenCalledWith('foo@bar.com');
+    });
+  });
+
+  test('should encrypt file, trigger updateEncrypted and set 2sec interval audit update', async () => {
+    jest.useFakeTimers();
+    const timeout = 2000;
+    const triggerTimes = 5;
+    const file = { file: { name: 'foo.txt' }, data: 'biteArr' };
+    const client = 'clientVirttu';
+    const spy = jest.fn(() =>
+      Promise.resolve({ encryptedFile: 'encFile', policyId: 'foo1bar', type: 'someType' }),
+    );
+    const updateEncrypted = jest.fn();
+    const updateAuditEvents = jest.fn();
+    tdf.encrypt.mockImplementation(spy);
+    tdf.encrypt.mockImplementation(spy);
+    services.getAuditEvents.mockImplementation(() =>
+      Promise.resolve({ json: () => Promise.resolve({ data: 'someData' }) }),
+    );
+
+    const { container } = render(
+      <Document
+        file={file}
+        virtruClient={client}
+        userId="foo@bar.com"
+        updateEncrypted={updateEncrypted}
+        updateAuditEvents={updateAuditEvents}
+      />,
+    );
+    act(() => {
+      fireEvent.click(getByTestId(container, 'encryptFile'));
+    });
+
+    await wait(() => {
+      expect(spy).toHaveBeenCalledWith({
+        client,
+        fileData: file.data,
+        filename: file.file.name,
+        userEmail: 'foo@bar.com',
+        asHtml: true,
+      });
+    });
+    expect(updateEncrypted).toHaveBeenCalledWith({
+      payload: 'encFile',
+      name: `${file.file.name}.html`,
+      type: file.file.type,
+    });
+    expect(updateAuditEvents).toHaveBeenCalledTimes(0);
+    act(() => {
+      jest.runTimersToTime(timeout * triggerTimes);
+    });
+    await wait(() => {
+      expect(updateAuditEvents).toHaveBeenCalledTimes(triggerTimes);
+    });
   });
 });
