2e5695b369fc13c9477597ae0bec113ac84f9e05
Nathan Bakirci <nathan@virtru.com>
2019-07-18 10:44:05 -0400

    Lint

--- src/scenes/AuthSelect/AuthSelect.js
+++ src/scenes/AuthSelect/AuthSelect.js
@@ -1,10 +1,4 @@
 import React, { useState } from 'react';
-import {
-  FormBox,
-  FormBoxInstruction,
-  FormBoxAlternative,
-  FormBoxButton,
-} from 'components/FormBox/FormBox';
 import './AuthSelect.css';
 import Modal from '../../components/Modal/Modal';
 import Button from '../../components/Button/Button';
