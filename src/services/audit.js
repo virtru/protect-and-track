2c99090b5ea12fd9336662d6ad6b094808547ab6
Nathan Bakirci <nathan@virtru.com>
2019-07-23 11:37:57 -0400

    Use dynamic envs

--- src/services/audit.js
+++ src/services/audit.js
@@ -1,10 +1,12 @@
-// TODO: Replace with SDK
+import getEnvironment from 'constants/environment';
 
-const auditUrl = policyId =>
-  `https://audit-develop01.develop.virtru.com/api/messages?sort=timestamp:asc&objectId=${policyId}`;
+const { auditUrl } = getEnvironment();
+
+// TODO: Replace with SDK
+const buildAuditReq = policyId => `${auditUrl}?sort=timestamp:asc&objectId=${policyId}`;
 
 export async function getAuditEvents({ userId, appId, policyId }) {
-  return fetch(auditUrl(policyId), {
+  return fetch(buildAuditReq(policyId), {
     headers: {
       Authorization: `Virtru [["${appId}","${userId}"]]`,
       'Accept-Version': 'v2',
