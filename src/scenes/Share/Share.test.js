9d49624d0abdcfe8d0032c6c08af0c0ed1138456
Dave Mihalcik <dmihalcik@virtru.com>
2019-07-24 09:41:28 -0400

    Update Share.test.js

--- src/scenes/Share/Share.test.js
+++ src/scenes/Share/Share.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { cleanup, render, wait, fireEvent, getByTestId, act } from '@testing-library/react';
 import Share from './Share';
-import { SHARE_STATE } from 'constants/sharing';
+import { SHARE_STATE, SHARE_PROVIDERS } from 'constants/sharing';
 import gsuite from './services/gsuite';
 
 jest.mock('./services/gsuite');
@@ -24,21 +24,21 @@ describe('Share', () => {
     await wait(() => expect(gsuite.share).toHaveBeenCalledWith('fake-id', ['a', 'b']));
     // Make sure we update the share state after
     const doneState = {
-      state: 'shared',
+      state: SHARE_STATE.SHARED,
       id: 'fake-id',
       link: 'https://drive.google.com/open?id=fake-id',
       recipients: ['a', 'b'],
     };
     await wait(() =>
       expect(setShare).toHaveBeenCalledWith({
-        provider: 'googledrive',
+        provider: SHARE_PROVIDERS.GOOGLEDRIVE,
         providerState: doneState,
       }),
     );
     const onClose = jest.fn();
     rerender(
       <Share
-        share="googledrive"
+        share={SHARE_PROVIDERS.GOOGLEDRIVE}
         encrypted={file}
         recipients={['a', 'b']}
         providers={{ googledrive: doneState }}
