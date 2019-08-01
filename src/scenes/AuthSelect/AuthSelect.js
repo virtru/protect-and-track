4f5f60e4791a3110e2f05f139963c8fc59c6507d
Nathan Bakirci <nathan@virtru.com>
2019-08-01 13:12:32 -0400

    lint

--- src/scenes/AuthSelect/AuthSelect.js
+++ src/scenes/AuthSelect/AuthSelect.js
@@ -1,20 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './AuthSelect.css';
 import Modal from '../../components/Modal/Modal';
-import Button from '../../components/Button/Button';
-// import Virtru from 'utils/VirtruWrapper';
-
-const AUTH_STEPS = {
-  ENTER_EMAIL: 0,
-  CHOOSE_PROVIDER: 1,
-  ENTER_CODE: 2,
-};
-
-function validateEmail(email) {
-  // eslint-disable-next-line no-useless-escape
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
 
 function AuthSelect({ onClose, login }) {
   useEffect(() => {
--- src/store.js
+++ src/store.js
@@ -118,7 +118,7 @@ export default createStore({
 
 function getQueryParam(name, url) {
   if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, '\\$&');
+  name = name.replace(/[\[\]]/g, '\\$&'); // eslint-disable-line no-useless-escape
   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
     results = regex.exec(url);
   if (!results) return null;
