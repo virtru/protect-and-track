f0ddac359f6c39fb426580978fc420188b1cf18e
NathanAB <nathan@virtru.com>
2019-08-02 15:20:33 -0400

    DSAT-87 - Fix policy building (#77)
    * Fix policy editing by using policy.builder()
    * Update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [Unreleased](https://github.com/virtru/protect-and-track/compare/master...HEAD)
 
 - NO-REF: _patch_
+  - Fix policy building by using `policy.builder()`
+- NO-REF: _patch_
   - Use Audit from SDK
 
 ## [v1.0.0](https://github.com/virtru/protect-and-track/compare/v0.1.2...v1.0.0) - 2019-08-02
--- src/utils/VirtruWrapper.js
+++ src/utils/VirtruWrapper.js
@@ -21,9 +21,20 @@ function _pushAction(action) {
  *
  * @param {?object} opts
  */
-function policyBuilder(opts) {
-  const builder = new Virtru.PolicyBuilder(opts);
-  let actions = [`const policy = new Virtru.PolicyBuilder(${opts ? 'policy' : ''})`];
+function policyBuilder(existingPolicy) {
+  if (existingPolicy) {
+    _pushAction({
+      title: 'Get Policy Builder',
+      code: 'const builder = existingPolicy.builder();',
+    });
+  } else {
+    _pushAction({
+      title: 'Create Policy Builder',
+      code: 'const builder = new Virtru.PolicyBuilder();',
+    });
+  }
+  const builder = existingPolicy ? existingPolicy.builder() : new Virtru.PolicyBuilder();
+  let actions = ['const policy = builder'];
   // This proxy records all calls, then logs them to the UI on `build` invocations.
   return new Proxy(builder, {
     get(target, propKey, receiver) {
