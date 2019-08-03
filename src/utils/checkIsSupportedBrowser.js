43336666af7351a99d2ac2151c81c79c7dc30192
Art Klepchukov <aklepchukov@virtru.com>
2019-08-03 14:29:54 -0400

    disable Firefox browser detection
    for CI compile

--- src/utils/checkIsSupportedBrowser.js
+++ src/utils/checkIsSupportedBrowser.js
@@ -1,6 +1,6 @@
 export default () => {
   //https://jsfiddle.net/6spj1059/
-  const isFirefox = typeof InstallTrigger !== 'undefined';
+  // const isFirefox = typeof InstallTrigger !== 'undefined';
   const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
   return isChrome;
