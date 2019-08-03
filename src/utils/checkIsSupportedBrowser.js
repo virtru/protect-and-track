f5ecac1a2a1ae4c2840ef1d6314756606052e1dd
Arthur Klepchukov <44370126+avkvirtru@users.noreply.github.com>
2019-08-03 16:34:44 -0400

    Remove isFirefox check entirely per Dave's suggestion
    Co-Authored-By: Dave Mihalcik <38867245+dmihalcik-virtru@users.noreply.github.com>

--- src/utils/checkIsSupportedBrowser.js
+++ src/utils/checkIsSupportedBrowser.js
@@ -1,6 +1,5 @@
 export default () => {
   //https://jsfiddle.net/6spj1059/
-  // const isFirefox = typeof InstallTrigger !== 'undefined';
   const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
 
   return isChrome;
