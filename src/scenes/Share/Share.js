00190b6d63b658ee8ccf0f6a24f8d35c9e6443bb
Dave Mihalcik <dmihalcik@virtru.com>
2019-08-05 17:00:04 -0400

    ♻️ Share improvements!
    * migrate onebox and drive to the continuation-style I'm using for google drive.
    * Update failure to take 'during' parameter, which improves error messaging
    * Updates failure to include a link to a file if it was successfully uploaded.
    * Google errors were not being logged to console

--- src/scenes/Share/Share.js
+++ src/scenes/Share/Share.js
@@ -55,39 +55,54 @@ function ShareButton({ children, init, onClick, type }) {
 
 function ShareSelect({ setShare, file, recipients, fileName }) {
   const shareToDropBox = async () => {
-    try {
-      const state = s =>
-        setShare({
-          provider: SHARE_PROVIDERS.DROPBOX,
-          providerState: { state: s, recipients },
-        });
-      state(SHARE_STATE.AUTHORIZING);
-      const accessToken = await dropboxsuite.signIn();
-      state(SHARE_STATE.UPLOADING);
-      const uploadResponse = await dropboxsuite.upload(accessToken, file);
-      const link = 'https://www.dropbox.com/preview' + uploadResponse.path_lower;
-      await dropboxsuite.share(accessToken, uploadResponse.id, recipients);
+    let link, id, state, error;
+    const upstate = () =>
       setShare({
         provider: SHARE_PROVIDERS.DROPBOX,
         providerState: {
-          state: SHARE_STATE.SHARED,
-          id: uploadResponse.id,
-          link,
+          state,
           recipients,
+          ...(id && { id }),
+          ...(link && { link }),
+          ...(error && { error }),
         },
       });
+    try {
+      state = SHARE_STATE.AUTHORIZING;
+      upstate();
+      const accessToken = await dropboxsuite.signIn();
+
+      state = SHARE_STATE.UPLOADING;
+      upstate();
+      const uploadResponse = await dropboxsuite.upload(accessToken, file);
+      id = uploadResponse.id;
+      link = 'https://www.dropbox.com/preview' + uploadResponse.path_lower;
+
+      state = SHARE_STATE.SHARING;
+      upstate();
+      await dropboxsuite.share(accessToken, uploadResponse.id, recipients);
+
+      state = SHARE_STATE.SHARED;
+      upstate();
+
+      dropboxsuite.signOut();
     } catch (e) {
       console.warn({ type: 'Drive share failure', cause: e });
+      error = {
+        during: state,
+      };
       // TODO(DSAT-67) enhance error messages
-      const error = e.status === 409 ? `${fileName} already exists in your Dropbox.` : null;
-      setShare({
-        provider: SHARE_PROVIDERS.DROPBOX,
-        providerState: { state: SHARE_STATE.FAIL, error },
-      });
+      if (e.status === 409) {
+        error.message = `${fileName} already exists in your Dropbox.`;
+      } else if (e.status === 403) {
+        error.message = 'Dropbox is rate limiting access to this application or user';
+      }
+      state = SHARE_STATE.FAIL;
+      upstate();
     }
   };
   const shareToDrive = async () => {
-    let link, id, state, errorMessage;
+    let link, id, state, error;
     const upstate = () =>
       setShare({
         provider: SHARE_PROVIDERS.GOOGLEDRIVE,
@@ -96,7 +111,7 @@ function ShareSelect({ setShare, file, recipients, fileName }) {
           recipients,
           ...(id && { id }),
           ...(link && { link }),
-          ...(errorMessage && { error: errorMessage }),
+          ...(error && { error }),
         },
       });
     try {
@@ -114,6 +129,7 @@ function ShareSelect({ setShare, file, recipients, fileName }) {
       state = SHARE_STATE.UPLOADING;
       upstate();
       const uploadResponse = await gsuite.upload(file.name, file.payload);
+
       state = SHARE_STATE.SHARING;
       id = uploadResponse.result.id;
       link = `https://drive.google.com/open?id=${id}`;
@@ -121,13 +137,17 @@ function ShareSelect({ setShare, file, recipients, fileName }) {
       await gsuite.share(id, recipients);
       // TODO(DSAT-67) Validate responses
       // TODO(DSAT-14) Store permissions and don't sign out.
+
       state = SHARE_STATE.SHARED;
-      gsuite.signOut();
       upstate();
+      gsuite.signOut();
     } catch (e) {
-      const { error } = e;
-      if (error === 'popup_closed_by_user') {
-        errorMessage = 'Authorization popup window closed or disabled';
+      console.info({ type: 'google drive error', cause: e });
+      error = {
+        during: state,
+      };
+      if (e.error === 'popup_closed_by_user') {
+        error.message = 'Authorization popup window closed or disabled';
       } else {
         console.warn({ type: 'Drive share failure', cause: e });
       }
@@ -136,16 +156,31 @@ function ShareSelect({ setShare, file, recipients, fileName }) {
     }
   };
   const shareToOnedrive = async () => {
+    let link, id, state, error;
+    const upstate = () =>
+      setShare({
+        provider: SHARE_PROVIDERS.ONEDRIVE,
+        providerState: {
+          state,
+          recipients,
+          ...(id && { id }),
+          ...(link && { link }),
+          ...(error && { error }),
+        },
+      });
     try {
-      const state = s =>
-        setShare({
-          provider: SHARE_PROVIDERS.ONEDRIVE,
-          providerState: { state: s, recipients },
-        });
-      state(SHARE_STATE.AUTHORIZING);
+      state = SHARE_STATE.AUTHORIZING;
+      upstate();
       const token = await onedrive.signIn();
-      state(SHARE_STATE.SHARING);
+
+      state = SHARE_STATE.UPLOADING;
+      upstate();
       const uploadResponse = await onedrive.upload(token, file);
+
+      state = SHARE_STATE.SHARING;
+      id = uploadResponse.id;
+      link = 'https://onedrive.live.com/?id=' + uploadResponse.id;
+      upstate();
       // NOTE onedrive doesn't like sharing with yourself, so break this into two bits
       // The owner may sign into onedrive with another account, and they may share with
       // the onedrive owner explicitly, so maybe we should break this into one request per recipient.
@@ -153,24 +188,22 @@ function ShareSelect({ setShare, file, recipients, fileName }) {
         await onedrive.share(token, uploadResponse.id, recipients.slice(1));
       }
       await onedrive.share(token, uploadResponse.id, recipients.slice(0, 1));
+
+      state = SHARE_STATE.SHARED;
+      upstate();
       onedrive.signOut();
-      setShare({
-        provider: SHARE_PROVIDERS.ONEDRIVE,
-        providerState: {
-          state: SHARE_STATE.SHARED,
-          id: uploadResponse.id,
-          link: 'https://onedrive.live.com/?id=' + uploadResponse.id,
-          recipients,
-        },
-      });
     } catch (e) {
-      // TODO(DSAT-67) enhance error messages
-      setShare({
-        provider: SHARE_PROVIDERS.ONEDRIVE,
-        providerState: { state: SHARE_STATE.FAIL },
-      });
       console.info({ type: '1drive error', cause: e });
-      throw e;
+      error = {
+        during: state,
+      };
+      if (e.error === 'popup_closed_by_user') {
+        error.message = 'Authorization popup window closed or disabled';
+      } else {
+        console.warn({ type: 'Drive share failure', cause: e });
+      }
+      state = SHARE_STATE.FAIL;
+      upstate();
     }
   };
   return (
@@ -221,16 +254,40 @@ function Fail({ provider, providerState, setShare }) {
     e.preventDefault();
     setShare(false);
   };
+  const verbs = {
+    [SHARE_STATE.AUTHORIZING]: 'authorize the user with',
+    [SHARE_STATE.UPLOADING]: 'upload to',
+    [SHARE_STATE.SHARING]: 'share to',
+    [SHARE_STATE.SHARED]: 'disconnect from',
+  };
+  const { error, link } = providerState;
+  const uploaded =
+    error &&
+    error.during &&
+    providerState.link &&
+    (error.during === SHARE_STATE.SHARING || error.during === SHARE_STATE.SHARED);
+  const verb = verbs[error && error.during] || 'share with';
   return (
     <ShareContainer>
-      <Title>Couldn't share to {'  ' + SHARE_TITLES[provider]}</Title>
+      <Title>Couldn't {verb + '  ' + SHARE_TITLES[provider]}</Title>
       <div className="Share-center">
         <Ico type={provider} /> <Ico type="danger" />
       </div>
-      <p>Try 'Download' on the demo page to share via email or other means.</p>
-      {providerState.error && (
+      {uploaded ? (
+        <p>
+          We were unable to invite your recipients to view your file. You may continue to view{' '}
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            your file
+          </a>
+          .
+        </p>
+      ) : (
+        <p>Try 'Download' on the demo page to share via email or other means.</p>
+      )}
+
+      {error && error.message && (
         <p className="Share-Fail-explain">
-          <img alt="" src="danger-small.svg" className="ShareSelect-inline" /> {providerState.error}
+          <img alt="" src="danger-small.svg" className="ShareSelect-inline" /> {error.message}
         </p>
       )}
       <Button onClick={tryAgain} variant="alternateButton">
