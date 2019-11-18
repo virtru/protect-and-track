db3f41c406b89b837c1dd4ce3be6539cf08deebf
Ken Ho <kho@virtru.com>
2019-11-17 22:32:04 -0500

    avoid Docker error: invalid reference format: repository name must be lowercase - braces

--- .buildkite/scripts/e2e.sh
+++ .buildkite/scripts/e2e.sh
@@ -4,4 +4,4 @@ set -eu
 
 echo "+++ PWD: $PWD"
 
-docker run --rm -it $(env | cut -f1 -d= | sed 's/^/-e /') -v "$(PWD)/e2e:/virtru/e2e" -v "$(PWD)/reports:/virtru/reports" -v "$(PWD)/.buildkite/scripts:/virtru/e2e/scripts"  virtru/automated-test:latest pytest -v -s -n2 e2e --cucumberjson=reports/cucumber_report.json --cucumber-json-expanded --html=reports/general_report.html --self-contained-html
+docker run --rm -it $(env | cut -f1 -d= | sed 's/^/-e /') -v "${PWD}/e2e:/virtru/e2e" -v "${PWD}/reports:/virtru/reports" -v "${PWD}/.buildkite/scripts:/virtru/e2e/scripts"  virtru/automated-test:latest pytest -v -s -n2 e2e --cucumberjson=reports/cucumber_report.json --cucumber-json-expanded --html=reports/general_report.html --self-contained-html
