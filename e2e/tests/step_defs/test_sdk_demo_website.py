cef7599f6374121a3471f77540d33def5c2bf8a2
Ken Ho <kho@virtru.com>
2019-11-18 13:14:29 -0500

    current tests only for production

--- e2e/tests/step_defs/test_sdk_demo_website.py
+++ e2e/tests/step_defs/test_sdk_demo_website.py
@@ -7,7 +7,7 @@ from pytest_bdd import scenario
 
 
 @pytest.mark.since_app_version(version='6.46.0', app='SR')
-@pytest.mark.environments(['production', 'staging'])
+@pytest.mark.environments(['production'])
 @scenario('../features/sdk_website.feature', 'Decrypt TDF3 file in SR', example_converters=dict(filename=str))
 def test_decrypt_tdf3_file_in_sr(current_test):
     pass
