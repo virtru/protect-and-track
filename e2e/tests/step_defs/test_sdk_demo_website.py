d01296d82e036e7a73c9c77aec1fe43046878d86
Ben Stanley <bstanley@virtru.com>
2019-11-25 11:55:55 -0500

    PLAT-146 added first test encrypt file and protect

--- e2e/tests/step_defs/test_sdk_demo_website.py
+++ e2e/tests/step_defs/test_sdk_demo_website.py
@@ -5,9 +5,6 @@ from pytest_bdd import scenario
 
 # Scenarios
 
-
-@pytest.mark.since_app_version(version='6.46.0', app='SR')
-@pytest.mark.environments(['production'])
-@scenario('../features/sdk_website.feature', 'Decrypt TDF3 file in SR', example_converters=dict(filename=str))
-def test_decrypt_tdf3_file_in_sr(current_test):
+@scenario('../features/sdk_website.feature', 'Encrypt file in Protect and Track demo site', example_converters=dict(filename=str))
+def test_sdk_website_encrypt_file(current_test):
     pass
