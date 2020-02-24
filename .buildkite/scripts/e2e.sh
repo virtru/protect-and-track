0cd63f97c344e568b74e25a4ae229f3c34ba60c5
Dave Mihalcik <dmihalcik@virtru.com>
2020-02-24 17:26:15 -0500

    Update e2e.sh

--- .buildkite/scripts/e2e.sh
+++ .buildkite/scripts/e2e.sh
@@ -3,11 +3,14 @@
 set -eu
 
 # TEST_MARKER is the test suite that will be selected to run
-if [ -z "$TEST_MARKER" ]; then
+if [ -z "${TEST_MARKER:-}" ]; then
+  echo "TEST_ENVIRONMENT_NAME: ${TEST_ENVIRONMENT_NAME}; TEST_MARKER: ${TEST_MARKER}"
   dash_m=-m "$TEST_MARKER"
+else
+  echo "TEST_ENVIRONMENT_NAME: ${TEST_ENVIRONMENT_NAME}"
+  dash_m=
 fi
 
-echo "TEST_ENVIRONMENT_NAME: ${TEST_ENVIRONMENT_NAME}; TEST_MARKER: ${TEST_MARKER}"
 
 docker run --rm $(env | cut -f1 -d= | sed 's/^/-e /') \
   virtru/automated-test:latest pytest \
