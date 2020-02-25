0e8c051f5fce4190d37c6fd63d82c3fc7190f6a3
Dave Mihalcik <dmihalcik@virtru.com>
2020-02-25 15:08:33 -0500

    add proxy support

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,5 @@
 import getQueryParam from 'utils/getQueryParam';
+import Virtru from 'utils/sdk';
 
 const develop01 = {
   authOptions: {
@@ -11,6 +12,15 @@ const develop01 = {
     easEndpoint: 'https://api-develop01.develop.virtru.com/accounts',
     acmEndpoint: 'https://api-develop01.develop.virtru.com/acm',
     readerUrl: 'https://secure-develop01.develop.virtru.com/start?htmlProtocol=1',
+  },
+  proxy: {
+    url: 'https://sdk.develop01.virtru.com/js/latest/proxy.html',
+    origins: [
+      'https://accounts.develop01.virtru.com',
+      'https://acm.develop01.virtru.com',
+      'https://eas.develop01.virtru.com',
+      'https://api.develop01.virtru.com',
+    ],
   },
 };
 
@@ -26,6 +36,15 @@ const develop02 = {
     acmEndpoint: 'https://api-develop02.develop.virtru.com/acm',
     readerUrl: 'https://secure-develop02.develop.virtru.com/start?htmlProtocol=1',
   },
+  proxy: {
+    url: 'https://sdk.develop02.virtru.com/js/latest/proxy.html',
+    origins: [
+      'https://accounts.develop02.virtru.com',
+      'https://acm.develop02.virtru.com',
+      'https://eas.develop02.virtru.com',
+      'https://api.develop02.virtru.com',
+    ],
+  },
 };
 
 const staging = {
@@ -39,6 +58,15 @@ const staging = {
     easEndpoint: 'https://api.staging.virtru.com/accounts',
     acmEndpoint: 'https://api.staging.virtru.com/acm',
     readerUrl: 'https://secure.staging.virtru.com/start?htmlProtocol=1',
+  },
+  proxy: {
+    url: 'https://sdk.staging.virtru.com/js/latest/proxy.html',
+    origins: [
+      'https://accounts.staging.virtru.com',
+      'https://acm.staging.virtru.com',
+      'https://eas.staging.virtru.com',
+      'https://api.staging.virtru.com',
+    ],
   },
 };
 
@@ -54,6 +82,7 @@ const production = {
     acmEndpoint: 'https://api.virtru.com/acm',
     readerUrl: 'https://secure.virtru.com/start?htmlProtocol=1',
   },
+  proxy: undefined,
 };
 
 const backendByParam = () => {
@@ -103,4 +132,8 @@ const config =
     ? backendByEnv()
     : backendByParam() || backendByEnv();
 
-export const { authOptions, clientConfig } = config;
+if (process.env.REACT_APP_VIRTRU_ENV !== 'production' && config.proxy && Virtru.XHRProxy) {
+  Virtru.XHRProxy.useProxyIfBrowser(config.proxy.url, config.proxy.origins);
+}
+
+export const { authOptions, clientConfig, proxy } = config;
