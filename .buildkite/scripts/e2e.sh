f44191b7c565809263301f1d10397bee3cccd8c4
Dave Mihalcik <dmihalcik@virtru.com>
2020-02-25 10:30:49 -0500

    set text_environment_name explicitly

--- .buildkite/scripts/e2e.sh
+++ .buildkite/scripts/e2e.sh
@@ -7,6 +7,13 @@ TEST_MARKER=${TEST_MARKER:-regression}
 
 # PYTEST_CONCURRENCY_NUM is the number of concurrent test runs
 PYTEST_CONCURRENCY_NUM=${PYTEST_CONCURRENCY_NUM:-"2"}
-echo "+++ TEST_ENVIRONMENT_NAME: $TEST_ENVIRONMENT_NAME; TEST_MARKER: $TEST_MARKER; PYTEST_CONCURRENCY_NUM: $PYTEST_CONCURRENCY_NUM"
+echo "TEST_ENVIRONMENT_NAME: $TEST_ENVIRONMENT_NAME; TEST_MARKER: $TEST_MARKER; PYTEST_CONCURRENCY_NUM: $PYTEST_CONCURRENCY_NUM"
 
-docker run --rm -it $(awk 'BEGIN{for(v in ENVIRON) print v}' | sed 's/^/-e /') -v "$PWD/e2e:/virtru/e2e" -v "$PWD/e2e/reports:/virtru/reports" -v "$PWD/.buildkite/scripts:/virtru/e2e/scripts"  virtru/automated-test:latest pytest -v -s -n$PYTEST_CONCURRENCY_NUM e2e -m $TEST_MARKER --cucumberjson=reports/cucumber_report.json --cucumber-json-expanded --html=reports/general_report.html --self-contained-html
+docker run --rm -it $(awk 'BEGIN{for(v in ENVIRON) print v}' | sed 's/^/-e /') \
+    -v "$PWD/e2e:/virtru/e2e" \
+    -v "$PWD/e2e/reports:/virtru/reports" \
+    -v "$PWD/.buildkite/scripts:/virtru/e2e/scripts" \
+    virtru/automated-test:latest \
+        pytest -v -s -n$PYTEST_CONCURRENCY_NUM \
+            e2e -m $TEST_MARKER \
+                --cucumberjson=reports/cucumber_report.json --cucumber-json-expanded --html=reports/general_report.html --self-contained-html
