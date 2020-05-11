fa055181a691b70d80be09a8d1c4b5ae68deb130
Cody Coggins ccoggins@virtru.com <ccoggins@virtru.com>
2020-05-11 16:45:04 -0400

    Add test env name, docker command fix.

--- .buildkite/scripts/e2e.sh
+++ .buildkite/scripts/e2e.sh
@@ -9,12 +9,12 @@ TEST_MARKER=${TEST_MARKER:-regression}
 PYTEST_CONCURRENCY_NUM=${PYTEST_CONCURRENCY_NUM:-"2"}
 echo "TEST_ENVIRONMENT_NAME: $TEST_ENVIRONMENT_NAME; TEST_MARKER: $TEST_MARKER; PYTEST_CONCURRENCY_NUM: $PYTEST_CONCURRENCY_NUM"
 
-docker run --rm -it $(env | cut -f1 -d= | sed 's/^/-e /')
--v "$PWD/e2e:/virtru/e2e" \
+docker run --rm -it $(env | cut -f1 -d= | sed 's/^/-e /') \
+  -v "$PWD/e2e:/virtru/e2e" \
   -v "$PWD/e2e/reports:/virtru/reports" \
   -v "$PWD/.buildkite/scripts:/virtru/e2e/scripts" \
   virtru/automated-test:latest \
   pytest -v -s -n$PYTEST_CONCURRENCY_NUM \
   -n2 e2e -m $TEST_MARKER \
-  --use-bp=chrome_store
---cucumberjson=reports/cucumber_report.json --cucumber-json-expanded --html=reports/general_report.html --self-contained-html
+  --use-bp=chrome_store \
+  --cucumberjson=reports/cucumber_report.json --cucumber-json-expanded --html=reports/general_report.html --self-contained-html
