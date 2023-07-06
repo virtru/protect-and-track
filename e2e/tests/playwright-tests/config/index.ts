00b2cfb8d53ab3ba2ef769c10f796733ffa5049f
Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>
2023-07-06 17:28:02 +0300

    debug

--- e2e/tests/playwright-tests/config/index.ts
+++ e2e/tests/playwright-tests/config/index.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import accounts from '../../../../accounts.json' assert {type: 'json'};
+import accounts from '../../../../accounts.json';
 
 export const userAuthData = {
 	mainUser: { // .auth-cks/user.json | Z|X
@@ -24,4 +24,29 @@ export const userAuthData = {
 		},
 	}
 
-};
+console.log('accounts', accounts);
+
+// export const userAuthData = {
+// 	mainUser: { // .auth-cks/user.json | Z|X
+// 		login: 'testerfour@trusteddataformat.org',
+// 		password: accounts['testerfour@trusteddataformat.org'].password,
+// 		email: 'cc_adminuser1@trusteddataformat.org',
+// 	},
+// 	secondUser: { // .auth-cks/user3.json |Y
+// 		login: 'test-user1@trusteddataformat.org',
+// 		password: accounts['test-user1@trusteddataformat.org'].password,
+// 	},
+// 	nonCKS: {
+// 		user1: { // .auth-non-cks/user1.json | user W
+// 			login: 'shareautomation01@gmail.com',
+// 			password: accounts['shareautomation01@gmail.com'].password,
+// 			email: 'testerfour@trusteddataformat.org'
+// 		},
+// 		user2: { // .auth-non-cks/user2.json | user Y
+// 			login: 'virtruqa3@gmail.com',
+// 			password: accounts['virtruqa3@gmail.com'].password,
+// 			email: 'testerfour@trusteddataformat.org'
+// 		},
+// 	}
+//
+// };
